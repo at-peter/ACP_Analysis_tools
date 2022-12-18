d70b19b2879ccf27856733f5b51fb2186c10be99
Peter home <atrazhev@ualberta.ca>
2022-12-18 14:02:26 -0700

    more 95% confidence analysis

--- conference_result_analysis.py
+++ conference_result_analysis.py
@@ -1,5 +1,22 @@
 import pandas as pd
 import statistics
+import scipy.stats as st
+
+
+def calculate_ninefive_confidence(value_array):
+    '''
+    This function calculates the 95% confidence interval of an array using the T distribution. 
+    It outputs the +- value 
+    '''
+    
+    nine_five = st.t.interval(confidence=0.95,
+    df=len(value_array)-1,
+    loc=statistics.mean(value_array),
+    scale=st.sem(value_array))
+    interval = statistics.mean(value_array)- nine_five[0] 
+
+    return interval
+
 
 list_of_ia2c_paths = [
     'C:\source\workshop\ia2c_2s-8x8-2p-2f-coop\ia2c_2s-8x8-2p-2f-coop.csv',
@@ -8,7 +25,7 @@ list_of_ia2c_paths = [
     'C:\source\workshop\ia2c_10x10-3p-3f-v0\ia2c_10x10-3p-3f-v0.csv'
 ]
 
-account = 'mappo'
+account = 'qmix'
 
 list_of_paths = [
     'C:/source/workshop/' + account + '_2s-8x8-2p-2f-coop/' + account + '_2s-8x8-2p-2f-coop.csv',
@@ -39,7 +56,13 @@ for path in list_of_paths:
         if i != 0:
             maxs.append(df[col].max())
         # print(df[col].max())
+    
+    mean_confidence = calculate_ninefive_confidence(means)
+    max_confidence = calculate_ninefive_confidence(maxs)
     print("++++++++++++++++++++++")
     print(name)
     print('Mean of means', statistics.mean(means))
+    print('Mean confidence interval',mean_confidence )
     print("Max results", max(maxs))
+    print('Max confidence values', max_confidence)
+
